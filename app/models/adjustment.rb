0efb87f82d3f461b1b5677f0db0c5358365bfd02
Simone Bravo <simonebravo96@gmail.com>
2024-02-26 15:23:32 +0100

    Add line items and adjustments implementation

--- app/models/adjustment.rb
+++ app/models/adjustment.rb
@@ -1,2 +1,12 @@
 class Adjustment < ApplicationRecord
+  belongs_to :order
+  belongs_to :line_item, optional: true
+
+  after_save :trigger_line_item_price_update
+
+  def trigger_line_item_price_update
+    return if line_item.nil?
+
+    line_item.save
+  end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,2 +1,12 @@
 class LineItem < ApplicationRecord
+  belongs_to :product
+  belongs_to :order
+
+  has_many :adjustments
+
+  before_save :calculate_final_price
+
+  def calculate_final_price
+    self.final_price = (price * quantity) - adjustments.pluck(:amount).sum
+  end
 end
